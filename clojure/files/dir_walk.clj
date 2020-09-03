11fc8c1fbd084ebf126bbc1b981a9ddf1ce7357e
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-09-03 19:47:49 +0200

    Add clojure dir walk example

---
+++ clojure/files/dir_walk.clj
@@ -0,0 +1,18 @@
+#!/usr/bin/env bb
+; uses babashka clojure to run
+; brew install borkdude/brew/babashka
+; see https://github.com/borkdude/babashka
+
+(defn get-files [dir]
+  (file-seq (clojure.java.io/file "/tmp")))
+
+
+(defn print-files [dir]
+  (doseq [file (get-files dir)]
+    (prn file)))
+
+(let [[dir] *command-line-args*]
+  (when (empty? dir)
+     (println "Usage: <directory>")
+     (System/exit -1))
+  (print-files dir))
