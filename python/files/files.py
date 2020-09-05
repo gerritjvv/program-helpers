7d9a05d609252671988b2f927fa679033715e25d
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-09-05 13:11:22 +0200

    current file of calling script

---
+++ python/files/files.py
@@ -0,0 +1,15 @@
+import inspect
+import os
+
+
+def current_file() -> str:
+    """
+    Return the absolute file of the script calling this function
+    e.g
+       /tmp/my.py calls current_file()  will return /tmp/my.py
+    see: https://stackoverflow.com/questions/13699283/how-to-get-the-callers-filename-method-name-in-python
+    """
+    frame = inspect.stack()[1]
+    filename = frame[0].f_code.co_filename
+
+    return os.path.abspath(filename)
