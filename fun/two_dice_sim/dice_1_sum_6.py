824c6fa16f97dfb2441e550f8157bc710662f141
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2020-09-18 14:56:40 +0200

    new dice sum sim

---
+++ fun/two_dice_sim/dice_1_sum_6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# We roll two dice (all 36 outcomes are equiprobable).
+# What is the conditional probability to have 1 on the first dice under the condition that the sum of two numbers is 6?
+# is => 1/30
+from random import choice
+
+
+def roll() -> bool:
+    return choice([1, 2, 3, 4, 5, 6])
+
+
+def experiment():
+    total = 100000
+    wins = 0
+
+    for _ in range(total):
+        a = roll()
+        b = roll()
+        if a == 1 and a + b == 6:
+            wins += 1
+
+    print(f"{wins}/{total} => {wins / total}")
+
+
+if __name__ == '__main__':
+    for _ in range(10):
+        experiment()
