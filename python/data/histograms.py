154e157823d5a8c10c82160904fa2c0a5bb31bea
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2021-01-30 21:12:54 +0100

    update hist plot to show normalised plot

--- python/data/histograms.py
+++ python/data/histograms.py
@@ -12,5 +12,16 @@ with open(file, 'rb') as io:
     # take a shape of (rows, columns)
     # and make 1 row and N columns appending each row's columns
     x_hist = np.squeeze(x.reshape(1,-1))
-    plt.hist(x, bins=1000)
+
+    print(f"SHAPE: {x_hist.shape}")
+
+    bins = 100
+
+    plot1 = plt.figure(1)
+    # histogram
+    plt.hist(x_hist, bins=bins)
+    # normalised histogram
+    plt.figure(2)
+    plt.hist(x_hist, bins=bins, density=1, weights=np.ones_like(x_hist)/x_hist.shape[0])
+
     plt.show()
