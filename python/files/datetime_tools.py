83a02d4e06c1f07bd71d15eab32e81d76e78014c
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2021-04-09 14:22:25 +0200

    add parser examples

--- python/files/datetime_tools.py
+++ python/files/datetime_tools.py
@@ -11,6 +11,16 @@ def format(d: datetime) -> str:
 
 print(format(datetime.utcnow()))
 
-
 def parse_date(d: str) -> datetime:
     return dateutil.parser.parse(d)
+
+parse_date('2021-04-01')
+# datetime.datetime(2021, 4, 1, 0, 0)
+parse_date('2021/04/01')
+# datetime.datetime(2021, 4, 1, 0, 0)
+parse_date('01/04/2021')
+# datetime.datetime(2021, 1, 4, 0, 0)
+parse_date('01-04-2021')
+# datetime.datetime(2021, 1, 4, 0, 0)
+parse_date('April 1 2021')
+# datetime.datetime(2021, 4, 1, 0, 0)
